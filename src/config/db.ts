c28a3f4957206f657bc07a2efe436e7117f8095f
fernandocieri <fernandocieri@gmail.com>
2024-07-06 19:56:02 +0200

    Add db config file

---
+++ src/config/db.ts
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+export const queryDatabase = async (query: string, params?: any[]) => {
+  const client = await pool.connect();
+  try {
+    const res = await client.query(query, params);
+    return res.rows;
+  } finally {
+    client.release();
+  }
+};
