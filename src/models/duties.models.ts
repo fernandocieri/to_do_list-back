d6ba23883b656b9137f764ffb0685b17b0dd7b61
fernandocieri <fernandocieri@gmail.com>
2024-07-07 15:48:55 +0200

    Add done column to Duty

--- src/models/duties.models.ts
+++ src/models/duties.models.ts
@@ -1,9 +1,11 @@
 export default class Duty {
   readonly id: string;
   name: string;
+  done: boolean;
 
-  constructor(id: string, name: string) {
+  constructor(id: string, name: string, done: boolean) {
     this.id = id;
     this.name = name;
+    this.done = done;
   }
 }
