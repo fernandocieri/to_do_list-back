3a9f846e6156a817877b499dcd14682e9cc94e03
fernandocieri <fernandocieri@gmail.com>
2024-07-07 20:45:16 +0200

    Change port number

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import router from "./routes";
 
 const app = express();
 
-const port = process.env.PORT ?? 3003;
+const port = process.env.PORT ?? 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
